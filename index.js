742ed8736713af1c67a745aa439aaca31dca1d22
Sohail Shaik <shaiksohail0726@gmail.com>
2025-08-17 21:29:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(cors({
   origin: [
     "https://to-do-app-sksohail19.vercel.app",
-    "https://to-do-app-ten-lac.vercel.app"
+    "https://to-do-app-ten-lac.vercel.app",
+    "https://to-do-app-git-main-sksohail19.vercel.app"
   ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
