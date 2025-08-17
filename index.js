e7992e2eb4a695708a4f2ce7c29aa121cecb9c30
Sohail Shaik <shaiksohail0726@gmail.com>
2025-08-17 21:14:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors({
   ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'authToken']
 }))
 
 
